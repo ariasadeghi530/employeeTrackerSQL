297583753e2e8d51222a27f010c12e2fb3ff0c26
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-26 14:52:04 -0800

    added initial questions array

--- server.js
+++ server.js
@@ -1,20 +1,30 @@
 const db = require('./config/db.js');
 const prompt = require('inquirer').createPromptModule();
 
-cosnt initChoices = [
+const initChoices = [
   "View all employees",
-  ""
-]
+  "View all employees by department",
+  "View all employees by manager",
+  "Add employee",
+  "Remove employee",
+  "Update employee role",
+  "Update employee manager",
+  "Add role",
+  "Remove role",
+  "View all roles",
+  "Add department",
+  "Remove department",
+  "View all departments",
+  "View total utilized budget of a department"
+];
+
+
 prompt([
   {
     type: 'list',
     name: 'listlist',
     message: 'Pick a list',
-    choices: [
-      "a",
-      "m",
-      "n"
-    ]
+    choices: initChoices
 }
 ]).then(response => {
   console.log(response);
