9968f2e2389aa43e49ed5fcc1a65c6b8056e7294
Aria Sadeghi <ariasadeghi@gmail.com>
2020-02-25 19:57:02 -0800

    created schema for db

---
+++ db/schema.sql
@@ -0,0 +1,28 @@
+DELETE DATABASE company_db IF EXISTS;
+
+CREATE DATABASE company_db;
+
+USE company_db;
+
+CREATE TABLE department (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL NOT NULL,
+department_id INT NOT NULL,
+FOREIGN KEY (department_id) REFERENCES department(id) 
+);
+
+CREATE TABLE employee (
+id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL, 
+manager_id INT NOT NULL,
+FOREIGN KEY (role_id) REFERENCES role(id),
+FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
